b1d28cacba3e1859445adea6d7d718cecb907089
Yehor <keeelori@gmail.com>
2020-02-21 18:51:41 +0200

    bot v0.1

--- bot.py
+++ bot.py
@@ -1,7 +1,20 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 import configparser
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 updater = Updater(token=config['APP']['TELEGRAM_TOKEN'], use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
