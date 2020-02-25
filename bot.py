fe6835a1ff898b626a493ca4786ddc9d01800aea
Yehor <keeelori@gmail.com>
2020-02-25 22:19:24 +0200

    last state before moving to ConversationHandler

--- bot.py
+++ bot.py
@@ -3,10 +3,7 @@ import configparser
 from handlers import *
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-# config stores config.ini file content
+# config is used to extract bot token
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -16,15 +13,26 @@ updater = Updater(token=config['APP']['TELEGRAM_TOKEN'], use_context=True)
 # dispatcher processes what updater sent to it
 dispatcher = updater.dispatcher
 
-# handlers definition
+# start of handlers definition section
 # matching start command with function to execute
 handler_start = CommandHandler('start', start)
 
-# callback query is what returned by pressing InlineKeyboardButton.
+# callback query is what returned by pressing InlineKeyboardButton. Matching each button shown on /start command with
+# function to execute
 handler_show_nearest_point = CallbackQueryHandler(show_nearest_point, pattern='show_nearest_point')
+handler_show_how_to_prepare = CallbackQueryHandler(show_how_to_prepare, pattern='show_how_to_prepare')
+handler_add_point = CallbackQueryHandler(add_point, pattern='add_point')
+handler_help_project = CallbackQueryHandler(help_project, pattern='help_project')
+
+# matching each button for waste type with function to execute
+
+# end of handlers definition section
 
 # adding handler to the dispatcher
 dispatcher.add_handler(handler_start)
 dispatcher.add_handler(handler_show_nearest_point)
+dispatcher.add_handler(handler_show_how_to_prepare)
+dispatcher.add_handler(handler_add_point)
+dispatcher.add_handler(handler_help_project)
 
 updater.start_polling()
--- handlers.py
+++ handlers.py
@@ -2,13 +2,17 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from pymongo import MongoClient
 import configparser
 
+# config is used to extract remote mongo instance
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 mongo_client = MongoClient(config['DATABASE']['URL'])
 db = mongo_client.motiwaste_bot
 
-# define a method to execute for '/start' command
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(4)
+
+
+# method to execute for '/start' command
 def start(update, context):
     # buttons to display under the welcome message
     buttons = [[
@@ -24,13 +28,31 @@ def start(update, context):
     update.message.reply_text('Чим можу бути корисним?', reply_markup=reply_markup)
 
 
-def show_nearest_point(update, contect):
-    # extracting each category name and creating an array of buttons
+# method to execute on show_nearest_point button click
+def show_nearest_point(update, context):
+    # extracting each category name and creating an array of buttons with callback_data = type
     buttons = []
     for category in db.categories.find():
-        buttons.append([InlineKeyboardButton(text=category['name'], callback_data='show_nearest_point')])
+        buttons.append([InlineKeyboardButton(text=category['name'], callback_data='category_' + category['type'])])
 
     # create keyboard instance
     reply_markup = InlineKeyboardMarkup(buttons)
     # send message with categories as inline buttons
     update.effective_message.reply_text(text="Що здаємо на переробку?", reply_markup=reply_markup)
+
+
+# method to execute after user selects a category from the list provided by show_nearest_point method
+def show_chosen_category(update, context):
+    pass
+
+
+def show_how_to_prepare(update, context):
+    pass
+
+
+def add_point(update, context):
+    pass
+
+
+def help_project(update, context):
+    pass
