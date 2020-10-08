92ef0f854b4c9e2029e3fe61bff3e8039937a1b1
Yehor Kuzmin <keeelori@gmail.com>
2020-10-09 00:26:27 +0300

    both new and existing users are updated in the DB. lastActivity added

--- flow_entry.py
+++ flow_entry.py
@@ -1,6 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from conversation_states import *
 from db_connection import *
+import datetime
 
 # method to execute for '/start' command
 def start(update, context):
@@ -8,7 +9,10 @@ def start(update, context):
     #writing a user into db
     if (db.users.find_one({"_id": update.effective_chat.id}) == None):
         db.users.insert_one( { "_id": update.effective_chat.id, "username": update.effective_chat.username, "firstName": update.effective_chat.first_name, 
-            "lastName": update.effective_chat.last_name} )
+            "lastName": update.effective_chat.last_name, "lastActivity": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ) 
+    else: #updating user info if exists
+        db.users.replace_one({"_id": update.effective_chat.id}, { "username": update.effective_chat.username, "firstName": update.effective_chat.first_name, 
+            "lastName": update.effective_chat.last_name, "lastActivity": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} )
         
     # buttons to display under the welcome message
     buttons = [[
