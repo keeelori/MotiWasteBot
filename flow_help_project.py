980ff35ada5c54c1f8898d486493acac1779322a
Yehor Kuzmin <keeelori@gmail.com>
2020-10-24 20:51:15 +0300

    transaltion updates

--- flow_help_project.py
+++ flow_help_project.py
@@ -7,6 +7,6 @@ def button_help_project(update, context):
     ]
     reply_markup = InlineKeyboardMarkup(button)
 
-    update.effective_message.reply_text(text="Пункт прийому більше не працює? Хочеш допомогти проєкту? Ти можеш на написати нам пошту team.motiwaste@gmail.com або у телеграмі @yehorkuzmin", reply_markup=reply_markup)
+    update.effective_message.reply_text(text="Пункт прийому більше не працює? Хочеш допомогти проєкту? Ти можеш написати нам на пошту team.motiwaste@gmail.com або у телеграмі @yehorkuzmin", reply_markup=reply_markup)
 
     return FLOW_HELP_PROJECT
