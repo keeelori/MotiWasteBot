07f74d8fb1ecc11865541a6b880e5f9b0254a6d8
Yehor Kuzmin <keeelori@gmail.com>
2020-11-05 12:35:41 +0200

    translations update

--- flow_entry.py
+++ flow_entry.py
@@ -16,8 +16,8 @@ def start(update, context):
         
     # buttons to display under the welcome message
     buttons = [[
-        InlineKeyboardButton(text='Найближчий пункт приймоу та утилізації', callback_data='button_show_nearest_location')],
-        [InlineKeyboardButton(text='Як підготувати сміття до утлізації', callback_data='button_show_how_to_prepare')],
+        InlineKeyboardButton(text='Найближчий пункт прийому', callback_data='button_show_nearest_location')],
+        [InlineKeyboardButton(text='Як підготувати сміття до утилізації', callback_data='button_show_how_to_prepare')],
         [InlineKeyboardButton(text='Додати пункт прийому вторсировини', callback_data='button_add_point')],
         [InlineKeyboardButton(text='Зв\'язатися з нами', callback_data='button_help_project')
          ]]
